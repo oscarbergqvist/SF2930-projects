1d2a1a30ff18c6c3cf814d79a4904bf0aef6bb89
Jessika Karlsson <jessika.karlsson@nepa.com>
2019-01-23 15:50:52 +0100

    Added regression model for men and created respopnse variable

--- project-1/main.R
+++ project-1/main.R
@@ -2,15 +2,20 @@
 library("dplyr")
 library("tidyverse")
 
-# load data for women
 setwd("/Users/Jessika/Documents/GitHub/SF2930-projects/project-1")
+
+# load data for women
 women <- read.csv("bodyfatwomen.csv")
-view(women)
 
 model_women <- lm(DEXfat ~ age + waistcirc + hipcirc + elbowbreadth + kneebreadth + anthro3a + anthro3b + anthro3c + anthro4, data = women)
+anova(model_women)
 
 # load data for men
 men <- read.csv("bodyfatmen.csv")
-view(men)
 
-mean_density <- mean(men$density)
+#create response variable using Siris Equation
+men$bfm <- c(495/(men$density)-450)
+
+#fit model to data
+model_men <- lm(bfm ~ age + weight + height + neck + chest + abdomen + hip + thigh + knee + ankle + biceps + forearm + wrist, data = men)
+anova(model_men)
