c4b0965e4de857882f2917ae61fa4e5244113f48
Jessika Karlsson <jessika.karlsson@nepa.com>
2019-01-23 14:46:03 +0100

    Update project-1/main.R
    Created two dataframes, one for each gender

--- project-1/main.R
+++ project-1/main.R
@@ -1,10 +1,13 @@
 # load packages
 library("dplyr")
+library("tidyverse")
 
 # load data for women
 library("TH.data")
 data("bodyfat")
+women <- bodyfat
 
 # load data for men
 library("mfp")
 data("bodyfat")
+men <- bodyfat
