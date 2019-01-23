a0e2e307bb16a8acf88d28aee3a6a7e60a765a01
Jessika Karlsson <jessika.karlsson@nepa.com>
2019-01-23 16:03:32 +0100

    added plot and summary to both models

--- project-1/main.R
+++ project-1/main.R
@@ -9,6 +9,8 @@ women <- read.csv("bodyfatwomen.csv")
 
 model_women <- lm(DEXfat ~ age + waistcirc + hipcirc + elbowbreadth + kneebreadth + anthro3a + anthro3b + anthro3c + anthro4, data = women)
 anova(model_women)
+summary(model_women)
+plot(model_women)
 
 # load data for men
 men <- read.csv("bodyfatmen.csv")
@@ -19,3 +21,5 @@ men$bfm <- c(495/(men$density)-450)
 #fit model to data
 model_men <- lm(bfm ~ age + weight + height + neck + chest + abdomen + hip + thigh + knee + ankle + biceps + forearm + wrist, data = men)
 anova(model_men)
+summary(model_men)
+plot(model_men)
