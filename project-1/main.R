f9cbc5d7d99610746e9fda3b2b17d0ab47cb76dc
Jessika Karlsson <jessika.karlsson@nepa.com>
2019-01-23 15:32:25 +0100

    First regression model added

--- project-1/main.R
+++ project-1/main.R
@@ -7,6 +7,10 @@ setwd("/Users/Jessika/Documents/GitHub/SF2930-projects/project-1")
 women <- read.csv("bodyfatwomen.csv")
 view(women)
 
+model_women <- lm(DEXfat ~ age + waistcirc + hipcirc + elbowbreadth + kneebreadth + anthro3a + anthro3b + anthro3c + anthro4, data = women)
+
 # load data for men
 men <- read.csv("bodyfatmen.csv")
+view(men)
 
+mean_density <- mean(men$density)
