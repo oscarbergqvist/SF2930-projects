54f86b795b88e38b55e5c21bc00bb474803f2218
Jessika Karlsson <jessika.karlsson@nepa.com>
2019-01-23 16:18:36 +0100

    transformed height and weight in men to metric units

--- project-1/main.R
+++ project-1/main.R
@@ -25,6 +25,10 @@ men <- read.csv("bodyfatmen.csv")
 #create response variable using Siris Equation
 men$bfm <- c(495/(men$density)-450)
 
+#transform weight from pounds to kg
+men$weight <- c(453.6*men$weight)
+men$height <- c(2.54*men$height)
+
 #fit model to data
 model_men <- lm(bfm ~ age + weight + height + neck + chest + abdomen + hip + thigh + knee + ankle + biceps + forearm + wrist, data = men)
 anova(model_men)
