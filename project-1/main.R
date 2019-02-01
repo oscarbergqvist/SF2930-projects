16dc643f1fec55424d621af3882cf731e315fd52
Jessika Karlsson <jessika.karlsson@nepa.com>
2019-02-01 15:03:39 +0100

    Började lägga in analys om Multicollinearitet

--- project-1/main.R
+++ project-1/main.R
@@ -18,6 +18,7 @@ men$height <- c(2.54*men$height)
 
 #fit model to data
 model_men <- lm(bfm ~ age + weight + height + neck + chest + abdomen + hip + thigh + knee + ankle + biceps + forearm + wrist, data = men)
+model_men <- lm(bfm ~ ., data = men)
 anova(model_men)
 summary(model_men)
 plot(model_men)
@@ -29,3 +30,16 @@ men %>%
   corrplot.mixed()
 
 #Jämför exempel 3, 4 från övning 3 (script finns på Canvas)
+
+
+#Spectral Decmoposition (ORIGIANAL DATASET)
+
+X <- data.matrix(bodyfat, rownames.force = NA)
+X_prim <- t(X)
+X_corr <- X_prim %*% X
+eigen <- eigen(X_corr, symmetric = TRUE, only.values = FALSE, EISPACK = FALSE)
+
+lambda_max <- max(eigen$values)
+lambda_min <- min(eigen$values)
+
+k <- lambda_max %/% lambda_min
