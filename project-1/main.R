f17156872bb36cc17e3842f26da2c468682c5223
Jessika Karlsson <jessika.karlsson@nepa.com>
2019-02-15 13:27:25 +0100

    Ändrade enheter till SI

--- project-1/main.R
+++ project-1/main.R
@@ -9,21 +9,27 @@ setwd("/Users/Jessika/Documents/GitHub/SF2930-projects/project-1")
 # load data for men
 men <- read.csv("bodyfatmen.csv")
 
-#create response variable using Siris Equation
-men$bfm <- c(495/(men$density)-450)
-
 #transform weight from pounds to kg
-men$weight <- c(453.6*men$weight)
-men$height <- c(2.54*men$height)
+men$weight <- c(0.001*453.6*men$weight)
+men$height <- c(2.54*0.01*men$height)
+men$neck <- c(0.01*men$neck)
+men$chest <- c(0.01*men$chest)
+men$abdomen <- c(0.01*men$abdomen)
+men$hip <- c(0.01*men$hip)
+men$thigh <- c(0.01*men$thigh)
+men$knee <- c(0.01*men$knee)
+men$ankle <- c(0.01*men$ankle)
+men$biceps <- c(0.01*men$biceps)
+men$forearm <- c(0.01*men$forearm)
+men$wrist <- c(0.01*men$wrist)
 
 #fit model to data
-model_men <- lm(bfm ~ age + weight + height + neck + chest + abdomen + hip + thigh + knee + ankle + biceps + forearm + wrist, data = men)
-model_men <- lm(bfm ~ ., data = men)
+model_men <- lm(density ~ ., data = men)
 anova(model_men)
 summary(model_men)
 plot(model_men)
 
-# visualize correlation between different explanatory variables
+#visualize correlation between different explanatory variables
 men %>%
   dplyr::select(bfm,age,weight,height,neck,chest,abdomen,hip,thigh,knee,ankle,biceps,forearm,wrist) %>%
   cor %>%
