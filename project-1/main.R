26474ec3a363f622b643bdae89738a7934ce7aae
Jessika Karlsson <jessika.karlsson@nepa.com>
2019-02-15 14:43:33 +0100

    började lägga till om model selection

--- project-1/main.R
+++ project-1/main.R
@@ -3,13 +3,14 @@ library("car")
 library("dplyr")
 library("tidyverse")
 library("corrplot")
+library("leaps")
 
 setwd("/Users/Jessika/Documents/GitHub/SF2930-projects/project-1")
 
 # load data for men
 men <- read.csv("bodyfatmen.csv")
 
-#transform weight from pounds to kg
+#transform to SI units
 men$weight <- c(0.001*453.6*men$weight)
 men$height <- c(2.54*0.01*men$height)
 men$neck <- c(0.01*men$neck)
@@ -23,24 +24,31 @@ men$biceps <- c(0.01*men$biceps)
 men$forearm <- c(0.01*men$forearm)
 men$wrist <- c(0.01*men$wrist)
 
-#fit model to data
+#fit linear model to data
 model_men <- lm(density ~ ., data = men)
 anova(model_men)
 summary(model_men)
 plot(model_men)
+studres(model_men)
+
+#Remove row 39, which is deemed to be an outler
+men <- men[-c(39),]
+
+#fit linear model to data AGAIN
+model_men <- lm(density ~ ., data = men)
+anova(model_men)
+summary(model_men)
+plot(model_men)
+studres(model_men)
 
 #visualize correlation between different explanatory variables
 men %>%
-  dplyr::select(bfm,age,weight,height,neck,chest,abdomen,hip,thigh,knee,ankle,biceps,forearm,wrist) %>%
+  dplyr::select(age,weight,height,neck,chest,abdomen,hip,thigh,knee,ankle,biceps,forearm,wrist) %>%
   cor %>%
   corrplot.mixed()
 
-#Jämför exempel 3, 4 från övning 3 (script finns på Canvas)
-
-
 #Spectral Decmoposition (ORIGIANAL DATASET)
-
-X <- data.matrix(bodyfat, rownames.force = NA)
+X <- data.matrix(men, rownames.force = NA)
 X_prim <- t(X)
 X_corr <- X_prim %*% X
 eigen <- eigen(X_corr, symmetric = TRUE, only.values = FALSE, EISPACK = FALSE)
@@ -49,3 +57,10 @@ lambda_max <- max(eigen$values)
 lambda_min <- min(eigen$values)
 
 k <- lambda_max %/% lambda_min
+
+# NU KAN VI KONSTATERA ATT: PROBLEM MED MULTIKOLINJÄRITET, OCH MÅNGA REGRESSORS HAR INVERKAN PÅ VARIABELN (DENSITY)
+
+#Model selection mha leaps
+#Vi kan använda oss av leaps (istället för backward/forward selection) pga antalet modeller är förhållandevis lågt 2^K
+leaps = regsubsets(density ~ ., data = men, nbest=10)
+plot(leaps, scale="adjr2")
